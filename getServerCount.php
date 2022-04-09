5aad6958dc9fc484eaf10aceaa7c02427f0765ea
nildo <nildoadao@yahoo.com.br>
2022-04-09 16:06:49 -0300

    Ajuste api

--- getServerCount.php
+++ getServerCount.php
@@ -26,8 +26,13 @@ function close_connection($connection){
 
 switch ($_SERVER["REQUEST_METHOD"]){
     case 'GET':
-        $result = get_number_of_servers();
-        $response_data = json_encode($result);
+        $servers_count = array();
+        $servers_count['TOTAL'] = get_number_of_servers();
+        $servers_count['DEV'] = get_server_by_amb('DEV');
+        $servers_count['QA'] = get_server_by_amb('QA');
+        $servers_count['CERT'] = get_server_by_amb("CERT");
+        $servers_count['UAT'] = get_server_by_amb('UAT');
+        $response_data = json_encode( $servers_count);
         echo $response_data;
         break;
 
@@ -39,7 +44,7 @@ switch ($_SERVER["REQUEST_METHOD"]){
 
 function get_number_of_servers(){
     $connection = build_connection();
-    $query = "SELECT COUNT(*) FROM hostdb";
+    $query = "SELECT COUNT(primary_key) as total FROM hostdb";
     try{
         $statement = $connection->prepare($query);
 
@@ -65,7 +70,46 @@ function get_number_of_servers(){
         }
         $statement->close();
         close_connection($connection);
-        return $result[0]["COUNT(*)"];
+        return $result[0]['total'];
+
+    } catch(Exception $e) {
+        echo "Falha ao conectar no banco" . $e->getMessage();
+        http_response_code(500);
+        close_connection($connection);
+        exit();
+    } 
+}
+
+function get_server_by_amb($ambiente){
+    $connection = build_connection();
+    $query = "SELECT COUNT(primary_key) as total FROM hostdb WHERE ambiente=?";
+    try{
+        $statement = $connection->prepare($query);
+        $statement->bind_param("s", $ambiente);
+
+        if($statement === false){
+            echo "Falha ao conectar no banco";
+            http_response_code(500);
+            exit();
+        }
+        
+        $statement->execute();
+        $statement->store_result();
+        $result = array();
+
+        for( $i = 0; $i < $statement->num_rows; $i++ ){
+            $metadata = $statement->result_metadata();
+            $params = array();
+            while ($field = $metadata->fetch_field())
+            {
+                $params[] = &$result[$i][$field->name];
+            }
+            call_user_func_array(array($statement, 'bind_result'), $params);
+            $statement->fetch();
+        }
+        $statement->close();
+        close_connection($connection);
+        return $result[0]['total'];
 
     } catch(Exception $e) {
         echo "Falha ao conectar no banco" . $e->getMessage();

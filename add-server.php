6781a59bed7d50d2bce4f80cc1ea57d67e9da42c
nildo <nildoadao@yahoo.com.br>
2022-04-10 17:48:30 -0300

    Ajuste api

--- add-server.php
+++ add-server.php
@@ -5,12 +5,8 @@ include('db-helper.php');
 switch ($_SERVER["REQUEST_METHOD"]){  
 
     case 'POST':
-        if(check_request_body()){            
-            $result = add_server();
-            send_response(json_encode($result), 201);
-        }
-
-        else send_response("Solicitação mal formatada", 400);
+        $result = add_server();                      
+        send_response(json_encode($result), 201);
         break;
 
     default:

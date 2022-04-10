df7562d8204fe7130ad2d40fa8dab784c6f191c5
nildo <nildoadao@yahoo.com.br>
2022-04-10 12:04:09 -0300

    Ajuste api

--- get-servers-count.php
+++ get-servers-count.php
@@ -27,100 +27,84 @@ function close_connection($connection){
 switch ($_SERVER["REQUEST_METHOD"]){
     case 'GET':
         $servers_count = array();
-        $servers_count['TOTAL'] = get_number_of_servers();
-        $servers_count['DEV'] = get_server_by_amb('DEV');
-        $servers_count['QA'] = get_server_by_amb('QA');
-        $servers_count['CERT'] = get_server_by_amb("CERT");
-        $servers_count['UAT'] = get_server_by_amb('UAT');
-        header("Content-Type: application/json; charset=UTF-8");
+        $servers_count = get_number_of_servers();
         $response_data = json_encode( $servers_count);
-        echo $response_data;
+        send_response($response_data, 200);
         break;
 
     default:
-        http_response_code(405);
-        header("Content-Type: application/json; charset=UTF-8");
-        echo "Método não permitido";
+        send_response("Método não permitido", 405);
         break;
 }
 
+function send_response($content, $status_code){
+    header("Content-Type: application/json; charset=UTF-8");
+    http_response_code($status_code);
+    echo $content;
+}
+
 function get_number_of_servers(){
-    $connection = build_connection();
-    $query = "SELECT COUNT(*) as total FROM hostdb";
     try{
-        $statement = $connection->prepare($query);
+        $connection = build_connection();
+        $servers_count = array();
 
-        if($statement === false){
-            echo "Falha ao preparar query";
-            http_response_code(500);
-            exit();
-        }
-        
-        $statement->execute();
-        $statement->store_result();
-        $result = array();
+        $query = "SELECT COUNT(*) as total FROM hostdb WHERE ambiente=?";
+        $result = db_select($connection, $query, "s", "DEV");
+        $servers_count['DEV'] = $result[0]['total'];
 
-        for( $i = 0; $i < $statement->num_rows; $i++ ){
-            $metadata = $statement->result_metadata();
-            $params = array();
-            while ($field = $metadata->fetch_field())
-            {
-                $params[] = &$result[$i][$field->name];
-            }
-            call_user_func_array(array($statement, 'bind_result'), $params);
-            $statement->fetch();
-        }
-        $statement->close();
+        $query = "SELECT COUNT(*) as total FROM hostdb WHERE ambiente=?";
+        $result = db_select($connection, $query, "s", "QA");
+        $servers_count['QA'] = $result[0]['total'];
+
+        $query = "SELECT COUNT(*) as total FROM hostdb WHERE ambiente=?";
+        $result = db_select($connection, $query, "s", "UAT");
+        $servers_count['UAT'] = $result[0]['total'];
+
+        $query = "SELECT COUNT(*) as total FROM hostdb WHERE ambiente=?";
+        $result = db_select($connection, $query, "s", "CERT");
+        $servers_count['CERT'] = $result[0]['total'];
+
+        $servers_count['TOTAL'] = $servers_count['DEV'] + $servers_count['QA'] + $servers_count['UAT'] + $servers_count['CERT'];
         close_connection($connection);
-        return $result[0]['total'];
+        return $servers_count;
+        
 
     } catch(Exception $e) {
-        echo "Falha ao conectar no banco" . $e->getMessage();
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(500);
+        send_response("Falha ao conectar no banco" . $e->getMessage(), 500);
         close_connection($connection);
         exit();
     } 
 }
 
-function get_server_by_amb($ambiente){
-    $connection = build_connection();
-    $query = "SELECT COUNT(*) as total FROM hostdb WHERE ambiente=?";
-    try{
-        $statement = $connection->prepare($query);
-        $statement->bind_param("s", $ambiente);
+function db_select($connection, $query, $param_types, ...$params){
+    $statement = $connection->prepare($query);
 
-        if($statement === false){
-            echo "Falha ao preparar query";
-            http_response_code(500);
-            exit();
+    if($param_types != ""){
+        $bind_string = "";
+        $bind_string = $bind_string.join(", ", $params);
+        $statement->bind_param($param_types, $bind_string);
+    }
+
+    if($statement === false){
+        throw new Exception("Falha ao preparar a query");
+    }
+    
+    $statement->execute();
+    $statement->store_result();
+    $result = array();
+
+    for( $i = 0; $i < $statement->num_rows; $i++ ){
+        $metadata = $statement->result_metadata();
+        $params = array();
+        while ($field = $metadata->fetch_field())
+        {
+            $params[] = &$result[$i][$field->name];
         }
-        
-        $statement->execute();
-        $statement->store_result();
-        $result = array();
-
-        for( $i = 0; $i < $statement->num_rows; $i++ ){
-            $metadata = $statement->result_metadata();
-            $params = array();
-            while ($field = $metadata->fetch_field())
-            {
-                $params[] = &$result[$i][$field->name];
-            }
-            call_user_func_array(array($statement, 'bind_result'), $params);
-            $statement->fetch();
-        }
-        $statement->close();
-        close_connection($connection);
-        return $result[0]['total'];
-
-    } catch(Exception $e) {
-        echo "Falha ao conectar no banco" . $e->getMessage();
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(500);
-        close_connection($connection);
-        exit();
-    } 
+        call_user_func_array(array($statement, 'bind_result'), $params);
+        $statement->fetch();
+    }
+    $statement->close();
+    return $result;
 }
 
 ?>
